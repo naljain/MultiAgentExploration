c1a0ec8555d69f342b85f4dde8d186be5950da45
Nalini <nalini@brfm.in>
2024-12-06 19:11:31 -0500

    adding bloating, and worker fn in global planner

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from rotorpy.world import World
 from rotorpy.trajectories.minsnap import MinSnap
 import numpy as np
 
-world = World.grid_forest(n_rows=2, n_cols=2, width=0.5, height=3, spacing=4)
+world = World.grid_forest(n_rows=2, n_cols=2, width=1, height=3, spacing=4)
 sensor_parameters = {'angular_fov': 360, 'angular_resolution': 1, 'fixed_heading': True, 'noise_density': 0.005}
 # sensor_parameters = None
 num_agents = 3
@@ -19,10 +19,10 @@ t_offset = 0
 for i in range(num_agents):
     # x = [X, Y, Z]
     x0 = np.array([initial_positions[i][0], initial_positions[i][1], 0])
-    xf = np.array([final_positions[i][0], final_positions[i][1], 0])
+    xf = np.array([final_positions[i][0], final_positions[i][1],2])
     config_list.append((MinSnap(points=np.row_stack((x0, xf)), v_avg=1.0, verbose=False), t_offset))
     t_offset += 0.5
 
 # sim = MultiAgentSimulation(world=world, num_agents=num_agents, t_final=10, t_step=1/100, config_list="Cool")
 sim = MultiAgentSimulation(world=world, num_agents=num_agents, t_final=10, t_step=1/100, config_list=config_list)
-results = sim.run_sim(sensor_parameters=sensor_parameters, range_sensor_plot=False, visualize=False)
+results = sim.run_sim(sensor_parameters=sensor_parameters, range_sensor_plot=False, visualize=True)
