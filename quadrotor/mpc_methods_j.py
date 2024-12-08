a77010d3ee3f81dbe83968e436a6e11c37bcc722
Joshua Caswell <joshalcaswell@yahoo.com>
2024-12-08 15:58:06 -0500

    added draft dynamics to mpc_template

--- quadrotor/mpc_methods_j.py
+++
@@ -1,8 +0,0 @@
-def add_dynamics_constraint(self, prog, x, u, N, T):
-    state_for_rotorpy = {'x': x[0:3], 'v':x[3:6], 'q':x[6:10],}
-    input_for_rotorpy = {'cmd_motor_thrusts': u}
-    multirotor = self.vehicle
-    dynamics = multirotor.statedot(state_for_rotorpy, input_for_rotorpy, T)
-    for i in range(N-1):
-        prog.AddQuadraticCost((x[i]).T @ self.Q @ (x[i]) + (u[i]).T @ self.R @ (u[i]))
-    prog.AddQuadraticCost((x[N-1]).T @ self.Qf @ (x[N-1]))
