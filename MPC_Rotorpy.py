2a3ac43a19c7167d46cab7535666308fe982bc4f
Joshua Caswell <jac0119@mix.wvu.edu>
2024-12-12 23:55:46 -0500

    Runs but sucks really

---
+++ MPC_Rotorpy.py
@@ -0,0 +1,98 @@
+from rotorpy.vehicles.crazyflie_params import quad_params
+from rotorpy.vehicles.multirotor import Multirotor
+from quadrotor.Dynamics_Rotorpy import MPC_RotorPy
+from src.global_planner import GlobalPlanner
+import numpy as np
+import time
+from mpl_toolkits.mplot3d import Axes3D
+# plannar params
+agents = {1: 123}  # , 2: 343, 3: 4444, 4: 444245, 5:13434} # dict = {agent num : agent id}
+time_step = 1
+
+bloat_val = 4  # BLOAT_VAL > RADIUS OF DRONE
+unknown_travel = True
+senor_range = 3  # 30 cm
+map = np.loadtxt('src/test_map2')
+Planner = GlobalPlanner(map, agents, time_step, bloat_val, senor_range,
+                            unknown_travel)
+initial_pose = Planner.start_pos
+goal_pose = Planner.run_planner()
+traj = np.array([(1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1788.53 ,1788.53 ,1788.53 ,1788.53),
+                 (1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1788.53 ,1788.53 ,1788.53 ,1788.53),
+                 (1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1788.53 ,1788.53 ,1788.53 ,1788.53),
+                 (1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1788.53 ,1788.53 ,1788.53 ,1788.53)])
+
+# Create a multirotor object
+vehicle = Multirotor(quad_params, control_abstraction='cmd_motor_speeds', aero=True)
+# Create a MPC controller object
+controller = MPC_RotorPy(map, vehicle)
+
+# u = controller.compute_mpc_feedback(x0,traj)
+# contol_step = {'cmd_thrust': u[0], 'cmd_moment': u[1:]}
+#
+# next_x = (vehicle.step(x0, contol_step, 0.1))
+# u_next = controller.compute_mpc_feedback(next_x,traj)
+# u_control_next = {'cmd_thrust': u_next[0], 'cmd_moment': u_next[1:]}
+# print(u_next)
+# print('next_x_1', next_x)
+#
+# next_x_2 = (vehicle.step(next_x, u_control_next, 0.1))
+# u_next_2 = controller.compute_mpc_feedback(next_x_2,traj)
+# print('next_x_2', next_x_2)
+# print(u_next_2)
+# Vehicle Initial State
+vehicle.initial_state = {'x': np.array([0,0,0]),
+                                            'v': np.zeros(3,),
+                                            'q': np.array([0, 0, 0, 1]), # [i,j,k,w]
+                                            'w': np.zeros(3,),
+                                            'wind': np.array([0,0,0]),  # Since wind is handled elsewhere, this value is overwritten
+                                            'rotor_speeds': np.array([1788.53, 1788.53, 1788.53, 1788.53])}
+
+vehicle.initial_state = {'x': np.array([0.0001,0.0001,0.0001]),
+                                            'v': np.array([0.000001,0.0000001,0.0000001]),
+                                            'q': np.array([0.000001, 0.000001, 0.000001, 1]), # [i,j,k,w]
+                                            'w': np.array([0.0000001,0.000001,0.0000001]),
+                                            'wind': np.array([0,0,0]),  # Since wind is handled elsewhere, this value is overwritten
+                                            'rotor_speeds': np.array([1788.53, 1788.53, 1788.53, 1788.53])}
+vehicle.motor_noise = 0.0
+init_x = vehicle.initial_state
+u_mpc_next = controller.compute_mpc_feedback(init_x,traj)
+u_control_next = {'cmd_motor_speeds': u_mpc_next} # Negative keeps it from falling faster. 
+next_state = vehicle.step(init_x, u_control_next, 0.1)
+counter = 0
+to_plot = []
+while True:
+    counter +=1
+    # if counter > 3: break
+    u_mpc_next = controller.compute_mpc_feedback(next_state,traj)
+    u_control_next = {'cmd_motor_speeds': u_mpc_next}
+    next_state = vehicle.step(next_state, u_control_next, 0.1)
+    to_plot.append(next_state)
+    if counter > 200:
+        break
+# u_test = {'cmd_motor_speeds': np.array([1788.53, 1788.53, 1788.53, 1788.53])}
+# while True:
+#     counter +=1
+#     next_state = vehicle.step(next_state, u_test, 0.1)
+#     to_plot.append(next_state)
+#     print('Position:', next_state['x'])
+#     print('Angle:', next_state['q'])
+
+#     if counter > 30:
+#         break
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x_vals = [state['x'][0] for state in to_plot]
+y_vals = [state['x'][1] for state in to_plot]
+z_vals = [state['x'][2] for state in to_plot]
+
+ax.plot(x_vals, y_vals, z_vals, label='Trajectory')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.legend()
+
+plt.show()
---
+++ quadrotor/Dynamics_Rotorpy.py
@@ -0,0 +1,286 @@
+import numpy as np
+from pydrake.solvers import MathematicalProgram, SnoptSolver, SolverOptions
+from pydrake.symbolic import Expression, if_then_else, sqrt
+class MPC_RotorPy(object):
+    def __init__(self, map,  vehicle):
+        """
+        Use this constructor to save vehicle parameters, set controller gains, etc.
+        Parameters:
+            vehicle_params, dict with keys specified in a python file under /rotorpy/vehicles/
+
+        """
+        self.vehicle = vehicle
+        self.map = map
+        self.d_safe = 10
+        self.Q = np.diag([100, 100, 100, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1])
+
+        self.R = np.eye(4)*0.00000000000000000000000000001
+        self.thrust_d = np.array([1788.53, 1788.53, 1788.53, 1788.53])
+        self.tau_m = vehicle.tau_m
+        self.rotor_geometry = vehicle.rotor_geometry
+        self.k_eta = vehicle.k_eta
+        self.num_rotors = vehicle.num_rotors
+        self.k_flap = vehicle.k_flap
+        self.k_m = vehicle.k_m
+        self.rotor_dir = vehicle.rotor_dir
+        self.weight = vehicle.weight
+        self.mass = vehicle.mass
+        self.inertia = vehicle.inertia
+        self.inv_inertia = vehicle.inv_inertia
+        self.gravity = 9.81
+        self.rotor_speed_min = vehicle.rotor_speed_min
+        self.rotor_speed_max = vehicle.rotor_speed_max
+        self.drag_matrix = vehicle.drag_matrix
+        self.rotor_drag_matrix = vehicle.rotor_drag_matrix
+
+    def hat_map(cls, s):
+        """
+        Given vector s in R^3, return associate skew symmetric matrix S in R^3x3
+        In the vectorized implementation, we assume that s is in the shape (N arrays, 3)
+        """
+        if len(s.shape) > 1:  # Vectorized implementation
+            return np.array([[ np.zeros(s.shape[0]), -s[:,2],  s[:,1]],
+                             [ s[:,2],     np.zeros(s.shape[0]), -s[:,0]],
+                             [-s[:,1],  s[:,0],     np.zeros(s.shape[0])]])
+        else:
+            return np.array([[    0, -s[2],  s[1]],
+                             [ s[2],     0, -s[0]],
+                             [-s[1],  s[0],     0]])
+    # def einsum_replacement(cls, tensor_ijk, matrix_ik):
+    #     result_j = np.zeros(tensor_ijk.shape[1])
+        
+    #     for i in range(tensor_ijk.shape[0]):
+    #         for k in range(tensor_ijk.shape[2]):
+    #             result_j += tensor_ijk[i, :, k] * matrix_ik[i, k]
+        
+    #     return result_j
+    def einsum_replacement(cls, tensor_ijk, matrix_ik):
+        result_j = [Expression(0) for _ in range(tensor_ijk.shape[1])]
+        
+        for i in range(tensor_ijk.shape[0]):
+            for k in range(matrix_ik.shape[1]):
+                for j in range(tensor_ijk.shape[1]):
+                    result_j[j] += tensor_ijk[i, j, k] * matrix_ik[i, k]
+    
+        return result_j
+        
+    def add_intial_state_constraint(self, prog, x, x_current):
+        """
+        this x_current should come from position data of the agent, not
+        from waypoints as the prev agent could have ended not on the waypoint
+        for the prev traj segment
+        """
+
+        state = np.concatenate([x_current['x'], x_current['v'], x_current['q'], x_current['w']])
+        n_x = state.shape[0]
+        # print(n_x)
+        for i in range(n_x):
+            prog.AddBoundingBoxConstraint(state[i], state[i], x[0, i])
+    
+    def add_input_saturation_constraint(self, prog, x, u, N):
+        n_u = u.shape[1]
+
+        #in homework they have u_d here, not sure if we need that?
+        for j in range(n_u):
+            for i in range(N-1):
+                prog.AddBoundingBoxConstraint(self.rotor_speed_min, self.rotor_speed_max, u[i, j])
+
+    def symbolic_quadrotor_dynamics(self,x, u):
+        """
+        Define symbolic quadrotor dynamics for pydrake. Written for individual motor speeds or "cmd_motor_speeds" as control input.
+
+        Parameters:
+            mass: Mass of the quadrotor (float).
+            inertia: Inertia matrix (3x3 NumPy array).
+            gravity: Gravitational acceleration (default: 9.81).
+
+        Returns:
+            A tuple containing:
+                - Symbolic variables for state (p, v, theta, omega).
+                - Symbolic variables for control (F, tau).
+                - Symbolic expressions for state derivatives (state_dot).
+        """
+        # convert u into controls
+        
+        # Define symbolic state variables
+        p = x[0:3]  # Position: [x, y, z]
+        v = x[3:6]  # Velocity: [vx, vy, vz]
+        q = x[6:10] # Orientation: Quaterniom[i, j, k, w]
+        omega = x[10:13] # Angular velocity: [wx, wy, wz]
+        rotor_speed = x[13:17] # Rotor speeds: [w1, w2, w3, w4]
+
+        # Define symbolic control inputs
+        cmd_motor_speeds = u[0:4]  # Motor speeds: [w1, w2, w3, w4]
+
+        # Gravity vector
+        g = np.array([0, 0, -self.gravity])
+
+        # Rotation matrix from body to world frame (Quats)
+        i, j, k, w = q
+        # Normalize quaternion
+        q = np.array([i, j, k, w])
+        norm_q = sqrt(i**2 + j**2 + k**2 + w**2)
+        epsilon = Expression(1e-6)
+        default_quaternion = np.array([Expression(0), Expression(0), Expression(0), Expression(1)])
+        for i in range(4):
+            q[i] = if_then_else(
+                norm_q > epsilon,
+                q[i] / norm_q,
+            default_quaternion[i])
+        # q = q / (np.linalg.norm(q) + 0.0000001)
+
+        # R, Rotation Matrix
+        R = np.array([
+            [1 - 2*q[1]**2 - 2*q[2]**2, 2*q[0]*q[1] - 2*q[3]*q[2], 2*q[0]*q[2] + 2*q[1]*q[3]],
+            [2*q[0]*q[1] + 2*q[3]*q[2], 1 - 2*q[0]**2 - 2*q[2]**2, 2*q[1]*q[2] - 2*q[0]*q[3]],
+            [2*q[0]*q[2] - 2*q[1]*q[3], 2*q[1]*q[2] + 2*q[3]*q[0], 1 - 2*q[0]**2 - 2*q[1]**2]
+        ])
+        
+        # Rotor speed derivatives
+        rotor_accel = (1/self.tau_m)*(cmd_motor_speeds - rotor_speed)
+
+        # Position derivatives
+        x_dot = v*1
+        
+        # Orientation derivatives
+        (q0, q1, q2, q3) = q
+        G = np.array([[q3, q2, -q1, -q0],
+                      [-q2, q3, q0, -q1],
+                      [q1, -q0, q3, -q2]])
+        quat_dot = 0.5 * G.T @ omega
+        quat_err = np.sum(q**2) - 1
+        quat_err_grad = 2* q
+        quat_dot = quat_dot - 0.5 * quat_err * quat_err_grad
+
+        # Compute Body Wrench, No wind though
+        body_airspeed_vector = R.T@(v)
+        hat_body_rates = self.hat_map(omega)@(self.rotor_geometry.T)
+        local_airspeeds = body_airspeed_vector[:, np.newaxis] + hat_body_rates
+        # Thrust of each rotor, assuming that the rotors all point in the body z direction
+        T = np.array([0, 0, self.k_eta])[:, np.newaxis]*rotor_speed**2
+
+        # No aero, F that S
+        D = -np.linalg.norm(body_airspeed_vector)*self.drag_matrix@body_airspeed_vector
+        H = -rotor_speed*(self.rotor_drag_matrix@local_airspeeds)
+        M_flap = -self.k_flap*rotor_speed*((self.hat_map(local_airspeeds.T).transpose(2,0,1))@np.array([0,0,1])).T
+        # D = np.zeros(3,)
+        # H = np.zeros((3,self.num_rotors))
+        # M_flap = -self.k_flap*rotor_speed*((self.hat_map(hat_body_rates.T).transpose(2,0,1))@np.array([0,0,1])).T
+
+        # Compute moments due to rotor thrusts, again none of that air shit
+        # M_force = -self.einsum_replacement(self.hat_map(self.rotor_geometry), T+H)
+        M_force = [-x for x in self.einsum_replacement(self.hat_map(self.rotor_geometry), T+H)]
+        M_yaw = self.rotor_dir*(np.array([0,0,self.k_m])[:, np.newaxis]*rotor_speed**2)
+
+        # Sum all elements to compute the the total body wrench
+        FtotB = np.sum(T + H, axis=1) + D
+        MtotB = M_force + np.sum(M_yaw + M_flap, axis=1)
+
+        Ftot = R@FtotB
+
+        # Velocity Derviatices
+        v_dot = (self.weight + Ftot)/self.mass
+
+        #Angular Velocity derivative
+        w_hat = self.hat_map(omega)
+        w_dot = self.inv_inertia @ (MtotB - w_hat @(self.inertia @ omega))
+
+        state_dot = np.concatenate([x_dot, v_dot, quat_dot, w_dot, rotor_accel])
+
+        # return derivatives
+        return state_dot
+    
+    def add_dynamic_constraints(self, prog, x, u, T, N):
+        n_x = len(x[0])
+        for k in range(N-1):
+            xk = x[k]
+            # print('len of xk in dyan co', len(xk))
+            uk = u[k]
+            x_dot = self.symbolic_quadrotor_dynamics(xk, uk)
+            # xk_1 = xk + x_dot*T
+            xk_1 = [xk[i] + x_dot[i] * T for i in range(n_x)]
+            # print('len of xk+1 in dyan co', len(xk_1))
+            # prog.AddConstraint(xk_1 - xk == 0)
+            for i in range(n_x):
+                # Create formula Object for Contraint
+                constraint = (Expression(x[k+1][i]) == xk_1[i])
+                prog.AddConstraint(constraint)
+    #             # prog.AddConstraint((xk_1[i] - xk[i]), np.array([0]))
+    #             # prog.AddConstraint((xk_1[i] - xk[i]) == 0)
+    #             # prog.AddBoundingBoxConstraint(0, 0, xk_1[i] - xk[i], lb=np.array([0]), ub=np.array([0]))
+    #     # prog.AddLinearEqualityConstraint((x[k + 1]), xk_1)
+    # def add_dynamic_constraints(self, prog, x, u, T, N):
+    #     for i in range(N-1):
+    #         x_dot = self.symbolic_quadrotor_dynamics(x[i], u[i])
+    #         prog.AddConstraint(x[i+1] == (x[i] + x_dot * T))
+
+    def add_cost(self, prog, x, x_ref, u, N):
+        u_ref = 0
+        for k in range(N-1):
+            # print((x[k] - x_ref[k]).T @ self.Q @ (x[k] - x_ref[k]))
+            prog.AddQuadraticCost(
+                (x[k] - x_ref[k]).T @ self.Q @ (x[k] - x_ref[k]) + (u[k]).T @ self.R @ (u[k])
+            )
+            # prog.AddQuadraticCost(
+            #     (x[k] - x_ref[k]).T @ self.Q @ (x[k] - x_ref[k])
+            #     )
+
+        prog.AddQuadraticCost((x[N-1] - x_ref[N-1]).T @ self.Q @ (x[N-1] - x_ref[N-1]))
+
+    def compute_mpc_feedback(self, x_current, x_ref):
+        # x_test = np.array([0, 0, 0.1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1788.53, 1788.53, 1788.53, 1788.53])
+        # u_test = np.array([0, 0, 0, 0])
+        # self.symbolic_quadrotor_dynamics(x_test, u_test)
+
+        current_state = np.concatenate([x_current['x'], x_current['v'], x_current['q'], x_current['w'], x_current['rotor_speeds']])
+        # QP params
+        N = 4  # prediction horizon TODO NEEDS TO BE TUNED
+        T = 0.1 # time step
+
+        # initialise mathematical program
+        prog = MathematicalProgram()
+
+        # initialise decision variables
+        x = np.zeros((N, 17), dtype= "object")
+        for i in range(N):
+            x[i] = prog.NewContinuousVariables(17, 'x_' + str(i))
+        u = np.zeros((N-1, 4), dtype = "object")
+        for i in range(N-1):
+            u[i] = prog.NewContinuousVariables(4, 'u_' + str(i))
+
+        # add constraints
+        self.add_intial_state_constraint(prog, x, x_current)
+
+        self.add_input_saturation_constraint(prog, x, u, N)
+
+        # Add dynamic constraints
+        self.add_dynamic_constraints(prog, x, u, T, N)
+
+
+        # add cost
+        # self.add_barrier_obstacle_constraint(prog, x, N)
+        # TODO input x_ref
+        self.add_cost(prog, x, x_ref, u, N)
+
+        # solve the QP
+        solver = SnoptSolver()
+        solver_id = solver.id()
+        prog.SetSolverOption(solver_id=solver_id, solver_option='Print file', option_value='./snopt.out')
+        # prog.SetInitialGuess(u, np.array([self.thrust_d],
+        #                                  [self.thrust_d],
+        #                                  [self.thrust_d],
+        #                                  [self.thrust_d]))
+        prog.SetInitialGuess(x, [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1788.53, 1788.53, 1788.53, 1788.53],
+                         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1788.53, 1788.53, 1788.53, 1788.53],
+                         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1788.53, 1788.53, 1788.53, 1788.53],
+                         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1788.53, 1788.53, 1788.53, 1788.53]])
+        result = solver.Solve(prog)
+        if result.is_success():
+            print("Solution found!")
+        else:
+            print("Solver failed.")        # get u_mpc
+        # u_mpc = result.GetSolution(u[0])
+        u_mpc = result.GetSolution(u[0]) # this is from hw, so need to see if this is correct
+        print(u_mpc)
+        return u_mpc
+    # def symbolic_dynamics(x, u, rotorpy_model):
